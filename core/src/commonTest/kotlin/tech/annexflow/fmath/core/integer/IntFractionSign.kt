9d6de2abecae570162fd7eb6652888cee3a4bb01
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2023-12-03 23:07:46 +0300

    test: add tests to check int fraction sign.

---
+++ core/src/commonTest/kotlin/tech/annexflow/fmath/core/integer/IntFractionSign.kt
@@ -0,0 +1,36 @@
+package tech.annexflow.fmath.core.integer
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Copyright 2023, @lavmee and the project contributors
+ **/
+
+class IntFractionSign {
+
+    @Test
+    fun zero() {
+        assertEquals(expected = 0, actual = fractionOf(0, 1).sign)
+    }
+
+    @Test
+    fun negative() {
+        val expected: Byte = -1
+        assertEquals(expected = expected, fractionOf(-1, 1).sign)
+        assertEquals(expected = expected, fractionOf(1, -1).sign)
+    }
+
+    @Test
+    fun positive() {
+        val expected: Byte = 1
+        assertEquals(expected = expected, fractionOf(1, 1).sign)
+        assertEquals(expected = expected, fractionOf(-1, -1).sign)
+    }
+
+    @Test
+    fun bigValues() {
+        assertEquals(expected = 1, fractionOf(2147483647, 2).sign)
+        assertEquals(expected = 1, fractionOf(2, 2147483647).sign)
+    }
+}
